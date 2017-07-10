a8d150e2b340bb11e77cb193535058146db123db
kangsanChang <rkdtks1005@gmail.com>
2017-07-10 16:21:57 +0900

    ADD findAll, create

---
+++ README.md
@@ -0,0 +1,1 @@
+# MariaDB Resftul Example
--- routes/user.js
+++ routes/user.js
@@ -2,25 +2,26 @@ var express = require('express');
 var models = require('../models');
 var router = express.Router();
 
-router.route('/')
-  .get((req, res, next) => {
+// Find all user
+router.get('/', (req, res, next) => {
     models.User.findAll().then((results) => {
       res.json(results);
-      // findAll 사용하기 전에 확실히 존재하는 table인지 확인해야 함.
+      // findAll 사용하기 전에 확실히 존재하는 table인지 확인해야 함. 당연하지만, 틀리면 model 못찾음
     }).catch((err) => {
       next(1007);
     });
-  })
-  .post((req, res, next) => {
-    var options = req.fetchParameter(['user_id']);
-    if (req.checkParamErr(options)) return next(options);
-
-    models.User.create({username: options.username}).then((result) => {
-      res.json(result);
-    }).catch((err) => {
-      console.log(err);
-      next(1006);
-    });
   });
+  
+// Create user
+router.post('/', (req,res)=>{
+    models.User.create({
+        userID: req.body.userID,
+        password: req.body.password
+     }).then(result=>{
+        res.json(result);
+     }).catch(err => {
+        console.error(err);
+     });
+});
 
 module.exports = router;
